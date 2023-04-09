70a465298e902db8b2575ca1f26df23f6a4cecb6
Cameron Chandler <webstormcam@gmail.com>
2023-04-09 16:03:51 -0400

    Get the API to take my requests
    Now I just need to get the colors to output.

--- javascript/script.js
+++ javascript/script.js
@@ -1,17 +1,29 @@
-document.getElementById('colorPicker').onchange = function(){
-    console.log(this.value)
-}
+
+let colorPicked = document.getElementById('colorPicker')
+let scheme = document.getElementById('scheme')
+
 
 /// Will Accept the values that are given
-function renderColors(){
-    fetch(`https://www.thecolorapi.com/scheme?hex=0047AB&mode=analogic`)
+function renderColors(colorHex,pickedSelection){
+    fetch(`https://www.thecolorapi.com/scheme?hex=${colorHex}&mode=${pickedSelection}`)
       .then(res => res.json())
       .then(data =>{
         console.log(data)
+        console.log(colorHex,pickedSelection)
+       
       })
 }
 
+const formOfInfo = document.getElementById('formOfInfo').addEventListener('submit',function(e){
+    e.preventDefault()
+    renderColors(colorPicked.value.slice(1),scheme.value)
+}) 
 
-renderColors()
+
+renderColors(colorPicked.value.slice(1),scheme.value)
+
+
+
+
 
 
