ab82ded976b82335a36f1f837f09050fbec3e0c2
Cameron Chandler <webstormcam@gmail.com>
2023-04-09 09:42:27 -0400

    Update script.js

--- javascript/script.js
+++ javascript/script.js
@@ -4,7 +4,11 @@ document.getElementById('colorPicker').onchange = function(){
 
 
 function renderColors(){
-    fetch(`https://www.thecolorapi.com/scheme?hex=`)
+    fetch(`https://www.thecolorapi.com/scheme?hex=${pickedColor}&mode=${pickedMode}`)
+      .then(res => res.json())
+      .then(data =>{
+        console.log(data)
+      })
 }
 
 
