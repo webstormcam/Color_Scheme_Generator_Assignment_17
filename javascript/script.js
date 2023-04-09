af1bc1575f2cbc12730263c623945e6898d87d9b
Cameron Chandler <webstormcam@gmail.com>
2023-04-09 10:17:09 -0400

    Styling the select section

--- javascript/script.js
+++ javascript/script.js
@@ -2,13 +2,16 @@ document.getElementById('colorPicker').onchange = function(){
     console.log(this.value)
 }
 
-
+/// Will Accept the values that are given
 function renderColors(){
-    fetch(`https://www.thecolorapi.com/scheme?hex=${pickedColor}&mode=${pickedMode}`)
+    fetch(`https://www.thecolorapi.com/scheme?hex=0047AB&mode=analogic`)
       .then(res => res.json())
       .then(data =>{
         console.log(data)
       })
 }
+
+
+renderColors()
 
 
